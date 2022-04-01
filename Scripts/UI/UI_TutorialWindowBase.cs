ec821ecbf30b8749307be8774a5dd080dbc17fce
Jesse Santana <jessesantana92@gmail.com>
2022-04-01 14:20:51 -0300

    Added option for tutorial to be closed after holding a key

--- Scripts/UI/UI_TutorialWindowBase.cs
+++ Scripts/UI/UI_TutorialWindowBase.cs
@@ -8,15 +8,55 @@ namespace Blabbers.Game00
 {
 	public abstract class UI_TutorialWindowBase : MonoBehaviour
 	{
+		public Slider HoldSlider;
+		public AnimationCurve SliderCurve;
 		public readonly float TapAnywhereDelay = 3f;
 		[ReadOnly] public bool CanTapToDisableScreen;
 		public float duration;
 		public UnityEvent OnWindowClosed;
 		private void OnEnable()
 		{
+			ShowTapTextAfterDelay();
 		}
 
 		public abstract void ShowScreen(bool enable);
 
+		public void ShowTapTextAfterDelay()
+		{
+			duration = 0;
+			CanTapToDisableScreen = false;
+			HoldSlider.gameObject.SetActive(false);
+
+			StartCoroutine(Routine());
+			IEnumerator Routine()
+			{
+				yield return new WaitForSeconds(TapAnywhereDelay);
+				CanTapToDisableScreen = true;
+				HoldSlider.gameObject.SetActive(true);
+			}
+		}
+
+		private void Update()
+		{
+			if (CanTapToDisableScreen)
+			{
+				if (Input.anyKey)
+				{
+					duration += Time.deltaTime;
+					if (HoldSlider.value >= 1)
+					{
+						ShowScreen(false);
+						OnWindowClosed?.Invoke();
+						CanTapToDisableScreen = false;
+					}
+				}
+				else
+				{
+					duration -= Time.deltaTime;
+				}
+				duration = Mathf.Clamp01(duration);
+				HoldSlider.value = SliderCurve.Evaluate(duration);
+			}
+		}
 	}
 }
