4c200581ec5d460d0ec62734b53edd61105bb1ce
Jesse Santana <jessesantana92@gmail.com>
2022-04-09 00:23:49 -0300

    Added these scripts to the submodule instead of the game folders

---
+++ Scripts/UI/UI_LevelSelect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeauRoutine;
+using Blabbers.Game00;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LevelSelect : MonoBehaviour
+{
+    public float buttonDropInterval = 0.1f;
+    public float pathFadeInDuration = 1f;
+    public MotionTween startTween;
+    public Transform buttonsParent;
+    public Image pathImage; 
+    void OnEnable()
+    {
+        // Path fade in
+        var pathImageColor = pathImage.color;
+        pathImageColor.a = 0f;
+        pathImage.color = pathImageColor;
+        pathImage.DOFade(1f, pathFadeInDuration);
+        
+        // Start buttons animation
+        var buttonMotions = buttonsParent.GetComponentsInChildren<MotionTweenPlayer>();
+        var buttonAmount = buttonMotions.Length;
+        Routine.Start(Run());
+        IEnumerator Run()
+        {
+            for (int i = 0; i < buttonAmount; i++)
+            {
+                //yield return Routine.WaitSeconds(buttonDropInterval);
+                buttonMotions[i].gameObject.SetActive(false);
+            }
+            for (int i = 0; i < buttonAmount; i++)
+            {
+                yield return Routine.WaitSeconds(buttonDropInterval);
+                var buttonMotion = buttonMotions[i];
+                buttonMotion.gameObject.SetActive(true);
+                startTween.PlaySequence(buttonMotion, false);
+            }
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/UI/UI_TutorialController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Blabbers.Game00;
+
+public class UI_TutorialController : MonoBehaviour, ISingleton
+{
+
+    public bool IsEnabled = false;
+
+    public bool CheckTutorialEnabled()
+    {
+
+        IsEnabled = false;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).gameObject.activeSelf)
+            {
+                IsEnabled = true;
+                break;
+            }
+        }
+
+        return IsEnabled;
+    }
+
+
+
+    public void OnCreated()
+    {
+    }
+}
