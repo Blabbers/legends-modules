421b9b73ad789b4ce7ac77499655234ba18c55cf
Jessinho <jesse_9_2@yahoo.com.br>
2022-04-23 17:45:52 -0300

    Added build post process for lol_spec file transfering

---
+++ Plugins/Tools/BuildHooks/Editor/LegendsBuildPostProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class LegendsBuildPostProcess
+{
+    [PostProcessBuild(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        TransferLolSpecFile(pathToBuiltProject);
+    }
+    
+    private static void TransferLolSpecFile(string pathToBuiltProject)
+    {
+        Debug.Log($"Post build process: Will try to transfer lol_spec file.");
+        var originalPath = $"{pathToBuiltProject}/StreamingAssets/lol_spec.json" ;
+        var rootBuildPath = $"{pathToBuiltProject}/lol_spec.json";
+        // Moves the file
+        try
+        {
+            Directory.Move(originalPath, rootBuildPath);
+            Debug.Log($"Post build process: Success on transferring lol_spec file.");
+        }
+        catch
+        {
+            Debug.Log($"<color=red>Post build process: The file 'lol_spec.json' was not found on the build directory.</color>");
+        }
+    }
+}
