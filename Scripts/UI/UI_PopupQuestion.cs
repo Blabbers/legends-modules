0f7b63adcc869450b996c4438f9fb896c2cc740e
Jessinho <jesse_9_2@yahoo.com.br>
2022-04-22 21:47:35 -0300

    Added questions popup to the module

---
+++ Scripts/UI/UI_PopupQuestion.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using Blabbers.Game00;
+using System.Collections.Generic;
+using BeauRoutine;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Question
+{
+    public string questionDescriptionKey;
+    public CorrectOption correctAnswer;
+    public string[] answersKeys;
+}
+
+public class UI_PopupQuestion : UI_PopupWindow, ISingleton
+{
+    private CorrectOption CorrectOption;
+    public Transform PopupParent;
+    public Transform ButtonsParent;
+    public TextMeshProUGUI QuestionDescriptionText;
+    public List<TextMeshProUGUI> AnswerTests;
+
+    public MotionTweenPlayer starMotionPlayer;
+    public MotionTween motionStarCorrect, motionStarWrong;
+    public AudioSFX sfxExtraStar;
+
+    public bool ChoseCorrectly { get; private set; }
+
+    public void OnCreated() { }
+
+    public void ClickOption(int id)
+    {
+        var clickedBtnTransform = AnswerTests[id].transform.parent;
+        // Position star at the correct place
+        starMotionPlayer.gameObject.SetActive(true);
+        starMotionPlayer.transform.position = clickedBtnTransform.position;
+        var clickedBtn = clickedBtnTransform.GetComponent<Button>();
+
+        if (id == (int)CorrectOption)
+        {
+            Debug.Log("Correct");
+            motionStarCorrect.PlaySequence(starMotionPlayer);
+            sfxExtraStar.PlaySelectedIndex(1);
+            clickedBtn.image.DOColor(Color.green, 0.5f);
+            CorrectOptionMethod();
+        }
+        else
+        {
+            Debug.Log("Wrong");
+            motionStarWrong.PlaySequence(starMotionPlayer);
+            sfxExtraStar.PlaySelectedIndex(0);
+            clickedBtn.image.DOColor(Color.red, 0.5f);
+            WrongOptionMethod();
+        }
+    }
+
+    public void ShowQuestion(Question question)
+    {
+        base.ShowPopup();
+        AudioController.Instance.FadeGameplayVolume(0.1f);
+        AudioController.Instance.FadeMusicVolume(0.02f);
+        // Loads the question
+        this.QuestionDescriptionText.LocalizeText(question.questionDescriptionKey);
+        this.CorrectOption = question.correctAnswer;
+        // Disable all
+        foreach (var answerTexts in AnswerTests)
+        {
+            answerTexts.transform.parent.gameObject.SetActive(false);
+        }
+        // Enables the needed ones and loads them up
+        for (int i = 0; i < question.answersKeys.Length; i++)
+        {
+            var answerKey = question.answersKeys[i];
+            var textMesh = AnswerTests[i];
+            textMesh.LocalizeText(answerKey);
+            var buttonObject = textMesh.transform.parent.gameObject;
+            // Enable back the needed ones
+            buttonObject.gameObject.SetActive(true);
+
+            Routine.Start(Run());
+            IEnumerator Run()
+            {
+                var button = buttonObject.GetComponent<Button>();
+                button.interactable = false;
+                yield return Routine.WaitRealSeconds(3f);
+                button.interactable = true;
+            }
+        }
+
+        ShuffleAnswers();
+    }
+
+    public void ShuffleAnswers()
+    {
+        var indexes = new List<int>();
+        var items = new List<Transform>();
+        for (int i = 0; i < ButtonsParent.childCount; ++i)
+        {
+            indexes.Add(i);
+            items.Add(ButtonsParent.GetChild(i));
+        }
+
+        foreach (var item in items)
+        {
+            item.SetSiblingIndex(indexes[Random.Range(0, indexes.Count)]);
+        }
+    }
+
+    public void CorrectOptionMethod()
+    {
+        ChoseCorrectly = true;
+        ClosePopup();
+    }
+
+    public void WrongOptionMethod()
+    {
+        ChoseCorrectly = false;
+        ClosePopup();
+    }
+
+    void ClosePopup()
+    {
+        AudioController.Instance.FadeResetGameplayVolume();
+        AudioController.Instance.FadeResetMusicVolume();
+
+        Routine.Start(Run());
+        IEnumerator Run()
+        {
+            yield return Routine.WaitSeconds(1.0f);
+            base.HidePopup();
+        }
+
+        //PopupParent.DOScale(0.0f, 0.5f).OnComplete(() =>
+        //{
+        //    HidePopup();
+        //});
+
+    }
+}
+
+public enum CorrectOption
+{
+    OptionA, OptionB, OptionC, OptionD
+}
