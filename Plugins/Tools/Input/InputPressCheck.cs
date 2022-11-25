36c4443c7bf0759ce8602d6bf97885d8c3e6f207
Jessé Santana <jesse_9_2@yahoo.com.br>
2022-11-25 10:29:07 -0300

    Add more options to input check

--- Plugins/Tools/Input/InputPressCheck.cs
+++ Plugins/Tools/Input/InputPressCheck.cs
@@ -1,26 +1,71 @@
-using Blabbers.Game00;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using NaughtyAttributes;
+
+public enum InputComparison
+{
+	EqualsTo,
+	BiggerThan,
+	SmallerThan,
+}
+
+public enum InputType
+{
+	GetButtonDown,
+	GetAxis,
+}
 
 public class InputPressCheck : MonoBehaviour
 {
-    public string inputName;
-    public UnityEvent OnButtonDown;
+	public InputType inputType;	
+	
+	// Get Button Down
+	private bool editorShowButtonDown => inputType == InputType.GetButtonDown;	
+	[ShowIf(nameof(editorShowButtonDown))]
+	public string inputName;
+
+	// Get Axis
+	private bool editorShowGetAxis => inputType == InputType.GetAxis;
+	[ShowIf(nameof(editorShowGetAxis))]
+	public float axisValue;
+	[ShowIf(nameof(editorShowGetAxis))]
+	public InputComparison axisCheck;
+
+	// On Pressed generic event
+	public UnityEvent OnPressed;
 
 	void Update()
-    {
-		if (Input.GetButtonDown(inputName)) 
-        {
-            ButtonDownInvoke();
-        }
-
-    }
-
-    // Public for the possibility of external inspector re-use
-    public void ButtonDownInvoke()
 	{
-        if (!this.isActiveAndEnabled) return;
-        OnButtonDown?.Invoke();
-    }
+		if (inputType == InputType.GetButtonDown)
+		{
+			if (Input.GetButtonDown(inputName))
+			{
+				OnPressedInvoke();
+			}
+		}
+
+		if (inputType == InputType.GetAxis)
+		{
+			var axis = Input.GetAxis(inputName);
+			switch (axisCheck)
+			{
+				case InputComparison.EqualsTo:
+					if (axis == axisValue) OnPressedInvoke();
+					break;
+				case InputComparison.BiggerThan:
+					if (axis > axisValue) OnPressedInvoke();
+					break;
+				case InputComparison.SmallerThan:
+					if (axis < axisValue) OnPressedInvoke();
+					break;
+			}
+		}
+	}
+
+	// Public for the possibility of external inspector re-use
+	public void OnPressedInvoke()
+	{
+		if (!this.isActiveAndEnabled) return;
+		OnPressed?.Invoke();
+	}
 }
