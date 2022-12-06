0e5a0297910c8c724dc57634ea6bd75dee453807
Caio Barbosa <ccsb.21@gmail.com>
2022-12-06 11:12:12 -0300

    Changes to SceneLoader

--- Scripts/Utility/SceneLoader.cs
+++ Scripts/Utility/SceneLoader.cs
@@ -74,34 +74,86 @@ namespace Blabbers.Game00
 			}
 			else
 			{
+
                 //Gambeta pra build beta:
-                var currentScene = SceneManager.GetActiveScene().name;
-                switch (currentScene)
+                //var currentScene = SceneManager.GetActiveScene().name;
+                string previousScene, targetScene;
+                string[] split;
+
+                foreach (var scene in gameData.levelSelectOverrideScenes)
                 {
-                    case "level-1":
-                        LoadSceneByName("simulation-1");
-                        break;
-                    case "level-2":
-                        LoadSceneByName("simulation-2");
-                        break;
-                    case "level-3":
-                        LoadSceneByName("customization");
-                        break;
-                    case "level-4":
-                        LoadSceneByName("customization");
-                        break;
-                    case "level-5":
-                        LoadSceneByName("simulation-3");
-                        break;
-                    case "level-6":
-                        LoadSceneByName("cutscene-final");
-                        break;
-                    default:
-                        SceneManager.LoadScene($"{gameData.gameLevelTag}level-select");
-                        break;
+
+                    previousScene = ConvertScenePathToName(scene.previousScene.ScenePath);
+
+
+					Debug.Log($"currentScene: {SceneManager.GetActiveScene().name}" +
+						$"\n[{previousScene}]");
+
+                    if(previousScene == SceneManager.GetActiveScene().name)
+                    {
+
+						targetScene = ConvertScenePathToName(scene.targetScene.ScenePath);
+
+						Debug.Log($"activeScene: {SceneManager.GetActiveScene().name}\n" +
+	                    $"previousScene: [{previousScene}] | targetScene: [{targetScene}]");
+
+						LoadSceneByName(targetScene);
+                        return;
+					}
+
                 }
-            }
+
+				SceneManager.LoadScene($"{gameData.gameLevelTag}level-select");
+
+
+
+				//switch (currentScene)
+				//            {
+				//                case "level-1":
+				//                    LoadSceneByName("simulation-1");
+				//                    break;
+				//                case "level-2":
+				//                    LoadSceneByName("simulation-2");
+				//                    break;
+				//                case "level-3":
+				//                    LoadSceneByName("customization");
+				//                    break;
+				//                case "level-4":
+				//                    LoadSceneByName("customization");
+				//                    break;
+				//                case "level-5":
+				//                    LoadSceneByName("simulation-3");
+				//                    break;
+				//                case "level-6":
+				//                    LoadSceneByName("cutscene-final");
+				//                    break;
+				//                default:
+				//                    SceneManager.LoadScene($"{gameData.gameLevelTag}level-select");
+				//                    break;
+				//            }
+
+
+			}
         }
+
+
+        private string ConvertScenePathToName(string path)
+        {
+            string sceneName;
+            string[] split;
+			string[] split2;
+
+
+			split = path.Split('/');
+            sceneName = split[split.Length - 1];
+
+			split2 = sceneName.Split('.');
+
+
+            return split2[0];
+		}
+
+
 
         public void LoadMainMenuScene()
         {
