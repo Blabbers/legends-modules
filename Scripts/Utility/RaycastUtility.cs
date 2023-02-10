c77508e27f419920abcf47d86467d86c2ab950dc
Caio Barbosa <ccsb.21@gmail.com>
2023-02-10 11:52:29 -0300

    Added 3d method to RaycastUtility

--- Scripts/Utility/RaycastUtility.cs
+++ Scripts/Utility/RaycastUtility.cs
@@ -18,6 +18,17 @@ public static class RaycastUtility
 	}
 
 
+	public static bool RaycastCheck3D(Vector3 origin, Vector3 dir, float distance, LayerMask mask, out RaycastHit hit)
+	{
+		Ray ray = new Ray(origin, dir);
+		hit = new RaycastHit();
 
+		if (Physics.Raycast(ray, out hit, distance, mask))
+		{
+			return true;
+		}
+
+		return false;
+	}
 
 }
