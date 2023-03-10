9f04111eee4694f4fd5c4b45b89548b567480657
Caio Barbosa <ccsb.21@gmail.com>
2023-03-10 17:09:20 -0300

    Loading scene wip

---
+++ Scripts/UI/UI_LoadingScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using BeauRoutine;
+using Blabbers;
+using Blabbers.Game00;
+using Fungus;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class UI_LoadingScreen : MonoBehaviour
+{
+	public TextLocalized displayText;
+
+	private void OnEnable()
+	{
+		//Checar qual a cena atual ou previous e carregar o texto baseado nisso
+	}
+
+}
