d2b51c1137b828e6811858958edf44148a70f5f1
Jessé Santana <jessesantana92@gmail.com>
2023-04-12 14:56:55 -0300

    FPS counter now runs everywhere

--- ThirdParty/Plugins/FPS counter/FPS_Counter.cs
+++ ThirdParty/Plugins/FPS counter/FPS_Counter.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 
-#if UNITY_EDITOR || DEVELOPMENT_BUILD || UNITY_CLOUD_BUILD
+//#if UNITY_EDITOR || DEVELOPMENT_BUILD || UNITY_CLOUD_BUILD
 public class FPS_Counter : MonoBehaviour
 {
     /* Assign this script to any object in the Scene to display frames per second */
@@ -54,4 +54,4 @@ public class FPS_Counter : MonoBehaviour
         GUI.Label(new Rect(5, 5, textDimensions.x, textDimensions.y), fps.ToString("F2") + "FPS", textStyle);
     }
 }
-#endif
+//#endif
