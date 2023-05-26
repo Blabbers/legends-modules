484629a4dca0c30a30ece92ce397457470e19c8b
Caio Barbosa <ccsb.21@gmail.com>
2023-05-26 14:05:45 -0300

    Disabled TTS Preview

--- Scripts/Editor/TTSPreview/TTSPreview.cs
+++ Scripts/Editor/TTSPreview/TTSPreview.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public static class TTSPreview
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-    static void Init()
-    {
-        LocalizationExtensions.EditorPreviewTTS = PreviewTTSInEditor;
-    }
+    //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    //static void Init()
+    //{
+    //    LocalizationExtensions.EditorPreviewTTS = PreviewTTSInEditor;
+    //}
     public static void PreviewTTSInEditor(string key)
     {
 #if UNITY_EDITOR
         var text = LocalizationExtensions.EditorLoadFromLanguageJson(key, displayMessages: false);
         Debug.Log("TTS: ".Colored("cyan") + text);
-        WindowsVoice.speak(text);
+        //WindowsVoice.speak(text);
 #endif
     }
 }
