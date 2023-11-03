0b7fffb458fc041bc1d535eafd8da281635619d6
Jesse Santana <jessesantana92@gmail.com>
2023-11-03 14:20:18 -0300

    PlayAnimState command now can waaitUntilFinished

--- ThirdParty/Plugins/Fungus/Scripts/Commands/PlayAnimState.cs
+++ ThirdParty/Plugins/Fungus/Scripts/Commands/PlayAnimState.cs
@@ -1,66 +1,97 @@
 // This code is part of the Fungus library (https://github.com/snozbot/fungus)
 // It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
-﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
 namespace Fungus
 {
-    /// <summary>
-    /// Plays a state of an animator according to the state name.
-    /// </summary>
-    [CommandInfo("Animation", 
-                 "Play Anim State", 
-                 "Plays a state of an animator according to the state name")]
-    [AddComponentMenu("")]
-    public class PlayAnimState : Command 
-    {
-        [Tooltip("Reference to an Animator component in a game object")]
-        [SerializeField] protected AnimatorData animator = new AnimatorData();
+	/// <summary>
+	/// Plays a state of an animator according to the state name.
+	/// </summary>
+	[CommandInfo("Animation",
+				 "Play Anim State",
+				 "Plays a state of an animator according to the state name")]
+	[AddComponentMenu("")]
+	public class PlayAnimState : Command
+	{
+		[Tooltip("Reference to an Animator component in a game object")]
+		[SerializeField] protected AnimatorData animator = new AnimatorData();
 
-        [Tooltip("Name of the state you want to play")]
-        [SerializeField] protected StringData stateName = new StringData();
+		[Tooltip("Name of the state you want to play")]
+		[SerializeField] protected StringData stateName = new StringData();
 
-        [Tooltip("Layer to play animation on")]
-        [SerializeField] protected IntegerData layer = new IntegerData(-1);
+		[Tooltip("Layer to play animation on")]
+		[SerializeField] protected IntegerData layer = new IntegerData(-1);
 
-        [Tooltip("Start time of animation")]
-        [SerializeField] protected FloatData time = new FloatData(0f);
+		[Tooltip("Start time of animation")]
+		[SerializeField] protected FloatData time = new FloatData(0f);
 
-        #region Public members
+		[SerializeField] private bool waitUntilFinished = false;
 
-        public override void OnEnter()
-        {
-            if (animator.Value != null)
-            {
-                animator.Value.Play(stateName.Value, layer.Value, time.Value);
-            }
+		#region Public members
 
-            Continue();
-        }
+		public override void OnEnter()
+		{
+			var waitTime = 0f;
+			if (animator.Value != null)
+			{
+				animator.Value.Play(stateName.Value, layer.Value, time.Value);
 
-        public override string GetSummary()
-        {
-            if (animator.Value == null)
-            {
-                return "Error: No animator selected";
-            }
+				if (waitUntilFinished)
+				{
+					var clip = FindAnimation(animator.Value, stateName.Value);
+					waitTime = clip.length;
+				}
+			}
 
-            return animator.Value.name + " (" + stateName.Value + ")";
-        }
+			StartCoroutine(Routine());
+			IEnumerator Routine()
+			{
+				if (waitTime > 0f)
+				{
+					yield return new WaitForSeconds(waitTime);
+				}
+				Continue();
+			}
+		}
 
-        public override Color GetButtonColor()
-        {
-            return new Color32(170, 204, 169, 255);
-        }
+		public AnimationClip FindAnimation(Animator animator, string name)
+		{
+			foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
+			{
+				if (clip.name == name)
+				{
+					return clip;
+				}
+			}
 
-        public override bool HasReference(Variable variable)
-        {
-            return animator.animatorRef == variable || stateName.stringRef == variable || 
-                layer.integerRef == variable || time.floatRef == variable || 
-                base.HasReference(variable);
-        }
+			return null;
+		}
 
-        #endregion
-    }    
+		public override string GetSummary()
+		{
+			if (animator.Value == null)
+			{
+				return "Error: No animator selected";
+			}
+
+			return animator.Value.name + " (" + stateName.Value + ")";
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(170, 204, 169, 255);
+		}
+
+		public override bool HasReference(Variable variable)
+		{
+			return animator.animatorRef == variable || stateName.stringRef == variable ||
+				layer.integerRef == variable || time.floatRef == variable ||
+				base.HasReference(variable);
+		}
+
+		#endregion
+	}
 }
 
