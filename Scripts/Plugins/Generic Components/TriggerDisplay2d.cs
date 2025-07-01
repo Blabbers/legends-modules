8519fe8ce83e5af5da17081c93477eeb5b11a8ec
Caio Barbosa <ccsb.21@gmail.com>
2025-07-01 09:47:05 -0300

    Improve TriggerDisplay2d

--- Scripts/Plugins/Generic Components/TriggerDisplay2d.cs
+++ Scripts/Plugins/Generic Components/TriggerDisplay2d.cs
@@ -1,133 +1,158 @@
-using Blabbers;
-using NaughtyAttributes;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Blabbers;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class TriggerDisplay2d : MonoBehaviour
 {
-	public GenericTriggerDisplay triggerDisplay;
+    public GenericTriggerDisplay triggerDisplay;
 
+    [Button]
+    void GetTriggerData()
+    {
+        //if (!triggerDisplay.transform) triggerDisplay.transform = this.transform;
 
-	[Button]
-	void GetTriggerData()
-	{
-		//if (!triggerDisplay.transform) triggerDisplay.transform = this.transform;
+        if (!triggerDisplay.transform)
+        {
+            if (FindValidCollider())
+            {
+                triggerDisplay.GetTriggerData();
+            }
+        }
+        else
+        {
+            if (triggerDisplay.transform.gameObject.GetComponent<Collider2D>() == null)
+            {
+                if (FindValidCollider())
+                {
+                    triggerDisplay.GetTriggerData();
+                }
+            }
 
-		if (!triggerDisplay.transform)
-		{
-			if (FindValidCollider())
-			{
-				triggerDisplay.GetTriggerData();
-			}
-		}
-		else
-		{
-			if(triggerDisplay.transform.gameObject.GetComponent<Collider2D>() == null)
-			{
-				if (FindValidCollider())
-				{
-					triggerDisplay.GetTriggerData();
-				}
-			}
+            if (triggerDisplay.collider == null)
+            {
+                if (FindValidCollider())
+                {
+                    triggerDisplay.GetTriggerData();
+                }
+            }
+        }
+    }
 
-			if(triggerDisplay.collider == null)
-			{
-				if (FindValidCollider())
-				{
-					triggerDisplay.GetTriggerData();
-				}
-			}
-		}
+    bool FindValidCollider()
+    {
+        bool hasCollider = false;
 
-		
-	}
+        if (gameObject.GetComponent<Collider2D>() != null)
+        {
+            hasCollider = true;
+            triggerDisplay.transform = transform;
+        }
 
-	bool FindValidCollider()
-	{
-		bool hasCollider = false;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<Collider2D>() != null)
+            {
+                hasCollider = true;
+                triggerDisplay.transform = transform.GetChild(i);
 
-		if (gameObject.GetComponent<Collider2D>() != null)
-		{
-			hasCollider = true;
-			triggerDisplay.transform = transform;
-		}
+                break;
+            }
+        }
 
+        return hasCollider;
+    }
 
-		for (int i = 0; i < transform.childCount; i++)
-		{
-			if (transform.GetChild(i).GetComponent<Collider2D>() != null)
-			{
-				hasCollider = true;
-				triggerDisplay.transform = transform.GetChild(i);
-
-				break;
-			}
-		}
-
-		return hasCollider;
-
-	}
-
-	private void OnDrawGizmos()
-	{
-		triggerDisplay.DrawGizmos();
-	}
+    private void OnDrawGizmos()
+    {
+        triggerDisplay.DrawGizmos();
+    }
 }
-
-
-
 
 [Serializable]
 public class GenericTriggerDisplay
 {
+    [Header("Configs")]
+    public TriggerShape2d triggerShape;
+    public Color gizmosColor = Color.green;
+    public Color borderColor = Color.green;
 
-	[Header("Configs")]
-	public TriggerShape2d triggerShape;
-	public Color gizmosColor = Color.green;
-	public Color borderColor = Color.green;
+    [Header("Components")]
+    public Transform transform;
+    public Transform parent;
+    public Collider2D collider;
 
-	[Header("Components")]
-	public Transform transform;
-	public Transform parent;
-	public Collider2D collider;
-	[SerializeField] BoxCollider2D boxCollider;
-	[SerializeField] CircleCollider2D circleCollider;
+    [SerializeField]
+    BoxCollider2D boxCollider;
 
+    [SerializeField]
+    CircleCollider2D circleCollider;
 
-	public void GetTriggerData()
-	{
-		if (!transform) return;
-		if (!collider) collider = transform.GetComponent<Collider2D>();
-		if(!parent) parent = transform.parent;
+    public void GetTriggerData()
+    {
+        if (!transform)
+            return;
+        if (!collider)
+            collider = transform.GetComponent<Collider2D>();
+        if (!parent)
+            parent = transform.parent;
 
-		if (!parent) parent = transform;
+        if (!parent)
+            parent = transform;
 
-		if (transform.GetComponent<BoxCollider2D>() !=null) boxCollider = transform.GetComponent<BoxCollider2D>();
-		if (transform.GetComponent<CircleCollider2D>() != null) circleCollider = transform.GetComponent<CircleCollider2D>();
-	}
+        if (transform.GetComponent<BoxCollider2D>() != null)
+        {
+            boxCollider = transform.GetComponent<BoxCollider2D>();
+            triggerShape = TriggerShape2d.Box;
+        }
 
-	//public BoxCollider2D collider;
+        if (transform.GetComponent<CircleCollider2D>() != null)
+        {
+            circleCollider = transform.GetComponent<CircleCollider2D>();
+            triggerShape = TriggerShape2d.Circle;
+        }
+    }
 
+    //public BoxCollider2D collider;
 
-	public void DrawGizmos()
-	{
+    public void DrawGizmos()
+    {
+        if (boxCollider != null)
+        {
+            float scaleX = boxCollider.size.x * parent.localScale.x;
+            float scaleY = boxCollider.size.y * parent.localScale.y;
 
-		if (boxCollider != null)
-		{
+            if (this.transform.localScale.x > 0)
+            {
+                scaleX = boxCollider.size.x * parent.localScale.x * transform.localScale.x;
+                scaleY = boxCollider.size.y * parent.localScale.y * transform.localScale.y;
+            }
 
-			GizmosUtility.DrawWireRectangle(transform.position, new Vector2(boxCollider.size.x * parent.localScale.x, boxCollider.size.y * parent.localScale.y), borderColor, collider.offset);
-			GizmosUtility.DrawRectangle(transform.position, new Vector2(boxCollider.size.x * parent.localScale.x, boxCollider.size.y * parent.localScale.y), gizmosColor, collider.offset);
-		}
+            GizmosUtility.DrawWireRectangle2d(
+                transform.position,
+                new Vector2(scaleX, scaleY),
+                0,
+                borderColor
+            );
+            GizmosUtility.DrawRectangle(
+                transform.position,
+                new Vector2(scaleX, scaleY),
+                gizmosColor,
+                collider.offset
+            );
+        }
 
-		if (circleCollider != null)
-		{
-			GizmosUtility.DrawSphere(transform.position, circleCollider.radius, gizmosColor, new Vector3(0,0,10),0.25f);
-		}
-
-
-	}
-
+        if (circleCollider != null)
+        {
+            GizmosUtility.DrawSphere(
+                transform.position,
+                circleCollider.radius,
+                gizmosColor,
+                new Vector3(0, 0, 10),
+                0.25f
+            );
+        }
+    }
 }
-
