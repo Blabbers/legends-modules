b0af8dcb1fafd26ba08b3ff1ab1e43e9f03cbc6a
Jessinho <jessesantana92@gmail.com>
2025-07-11 11:44:14 -0300

    FollowWorldComponent: Make Target public

--- Scripts/UI/UI_FollowWorldComponent.cs
+++ Scripts/UI/UI_FollowWorldComponent.cs
@@ -1,65 +1,108 @@
-using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class UI_FollowWorldComponent : MonoBehaviour
 {
-	[Header("Components")]
-	[SerializeField] Transform target;
-	[SerializeField] Camera mainCam;
+    [Header("Components")]
+    [SerializeField]
+    public Transform target;
 
-	//[HorizontalLine(color: EColor.White)]
+    [SerializeField]
+    Camera mainCam;
 
-	[BoxGroup("Movement")][SerializeField] bool lerpToPos = false;
-	[BoxGroup("Movement")][ShowIf(nameof(lerpToPos))][SerializeField][MinValue(10)] float lerpSpeed;
+    //[HorizontalLine(color: EColor.White)]
 
-	[BoxGroup("Offset")][SerializeField] bool hasPositionOffset = true;
-	[BoxGroup("Offset")][ShowIf(nameof(hasPositionOffset))][SerializeField] Vector2 positionOffset;
+    [BoxGroup("Movement")]
+    [SerializeField]
+    bool lerpToPos = false;
 
-	[BoxGroup("Bounds")][SerializeField] bool clampAtBounds = true;
-	[BoxGroup("Bounds")][ShowIf(nameof(clampAtBounds))][SerializeField] [Range(0,0.25f)] float paddingX;
-	[BoxGroup("Bounds")][ShowIf(nameof(clampAtBounds))][SerializeField] [Range(0, 0.25f)] float paddingY;
-	//[ShowIf(nameof(clampAtBounds))][SerializeField] Vector2 boundsPadding = Vector2.zero;
+    [BoxGroup("Movement")]
+    [ShowIf(nameof(lerpToPos))]
+    [SerializeField]
+    [MinValue(10)]
+    float lerpSpeed;
 
-	Transform transformUI;
+    [BoxGroup("Offset")]
+    [SerializeField]
+    bool hasPositionOffset = true;
 
-	private void Awake()
-	{
-		transformUI = this.transform;
-		if (!mainCam) mainCam = Camera.main;
-	}
+    [BoxGroup("Offset")]
+    [ShowIf(nameof(hasPositionOffset))]
+    [SerializeField]
+    Vector2 positionOffset;
 
-	private void Update()
-	{
-		PositionUI();
-	}
+    [BoxGroup("Bounds")]
+    [SerializeField]
+    bool clampAtBounds = true;
 
+    [BoxGroup("Bounds")]
+    [ShowIf(nameof(clampAtBounds))]
+    [SerializeField]
+    [Range(0, 0.25f)]
+    float paddingX;
 
-	void PositionUI()
-	{
-		Vector2 targetPos;
+    [BoxGroup("Bounds")]
+    [ShowIf(nameof(clampAtBounds))]
+    [SerializeField]
+    [Range(0, 0.25f)]
+    float paddingY;
 
-		//targetPos = CameraToWorldUtility.WorldToCameraPosition_Clamped(target, mainCam, boundsPadding.x, boundsPadding.y);]
-		if(clampAtBounds)
-		{
-			targetPos = CameraToWorldUtility.WorldToCameraPosition_Clamped(target, mainCam, positionOffset.x, positionOffset.y, paddingX, paddingY);
-		}
-		else
-		{
-			targetPos = CameraToWorldUtility.WorldToCameraPosition(target, mainCam, positionOffset.x, positionOffset.y);
-		}
+    //[ShowIf(nameof(clampAtBounds))][SerializeField] Vector2 boundsPadding = Vector2.zero;
 
-		if (lerpToPos)
-		{
-			transformUI.position = Vector3.MoveTowards(transformUI.position, targetPos, Time.deltaTime * lerpSpeed);
-		}
-		else
-		{
-			transformUI.position = targetPos;
-		}
-		
-	}
+    Transform transformUI;
 
+    private void Awake()
+    {
+        transformUI = this.transform;
+        if (!mainCam)
+            mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        PositionUI();
+    }
+
+    void PositionUI()
+    {
+        Vector2 targetPos;
+
+        //targetPos = CameraToWorldUtility.WorldToCameraPosition_Clamped(target, mainCam, boundsPadding.x, boundsPadding.y);]
+        if (clampAtBounds)
+        {
+            targetPos = CameraToWorldUtility.WorldToCameraPosition_Clamped(
+                target,
+                mainCam,
+                positionOffset.x,
+                positionOffset.y,
+                paddingX,
+                paddingY
+            );
+        }
+        else
+        {
+            targetPos = CameraToWorldUtility.WorldToCameraPosition(
+                target,
+                mainCam,
+                positionOffset.x,
+                positionOffset.y
+            );
+        }
+
+        if (lerpToPos)
+        {
+            transformUI.position = Vector3.MoveTowards(
+                transformUI.position,
+                targetPos,
+                Time.deltaTime * lerpSpeed
+            );
+        }
+        else
+        {
+            transformUI.position = targetPos;
+        }
+    }
 }
