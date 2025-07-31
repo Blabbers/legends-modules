dcf8d038c0b41a781ae2a225096c0c3111915f16
Jessinho <jessesantana92@gmail.com>
2025-07-31 01:23:59 -0300

    Add new options for audio controller

--- Scripts/Plugins/Audio SFX/AudioController.cs
+++ Scripts/Plugins/Audio SFX/AudioController.cs
@@ -1,151 +1,159 @@
-﻿using DG.Tweening;
-using NaughtyAttributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Blabbers.Game00
 {
-	public class AudioController : MonoBehaviour, ISingleton
-	{
-		public static AudioController Instance => Singleton.Get<AudioController>();
+    public class AudioController : MonoBehaviour, ISingleton
+    {
+        public static AudioController Instance => Singleton.Get<AudioController>();
 
-		public AudioSource musicSource;
-		public AudioSource gameplaySource;
-		public bool isMuted = false;
+        public AudioSource musicSource;
+        public AudioSource gameplaySource;
+        public bool isMuted = false;
+        public bool playLobbyMusicOnStart = false;
 
-		private static float baseMusicVolume;
-		private static float baseGameplayVolume;
+        private static float baseMusicVolume;
+        private static float baseGameplayVolume;
 
-		public static float MusicVolumeFull => baseMusicVolume;
-		public static float MusicVolumeHalf => baseMusicVolume * 0.5f;
-		public static float MusicVolumeLow => baseMusicVolume * 0.15f;
+        public static float MusicVolumeFull => baseMusicVolume;
+        public static float MusicVolumeHalf => baseMusicVolume * 0.5f;
+        public static float MusicVolumeLow => baseMusicVolume * 0.1f;
 
-		[BoxGroup("Game Audio")]
-		//Music
-		public AudioClip gameplayMusic, lobbyMusic;
-		public Action<bool> OnMuteSFX;
+        [BoxGroup("Game Audio")]
+        //Music
+        public AudioClip gameplayMusic,
+            lobbyMusic;
+        public Action<bool> OnMuteSFX;
 
-		private void Awake()
-		{
-			baseMusicVolume = musicSource.volume;
-			baseGameplayVolume = gameplaySource.volume;
-		}
+        private void Awake()
+        {
+            baseMusicVolume = musicSource.volume;
+            baseGameplayVolume = gameplaySource.volume;
+        }
 
-		void ISingleton.OnCreated()
-		{
-		}
+        void Start()
+        {
+            if (playLobbyMusicOnStart)
+            {
+                PlayLobbyMusic();
+            }
+        }
 
-		public void PlayGameplayMusic()
-		{
-			PlayMusic(gameplayMusic);
-		}
+        void ISingleton.OnCreated() { }
 
-		public void PlayLobbyMusic()
-		{
-			PlayMusic(lobbyMusic);
-		}
+        public void PlayGameplayMusic()
+        {
+            Instance.PlayMusic(Instance.gameplayMusic);
+        }
 
-		public void PlayMusic(AudioClip musicClip)
-		{
-			if (musicClip.Equals(Instance.musicSource.clip)) return;
+        public void PlayLobbyMusic()
+        {
+            Instance.PlayMusic(Instance.lobbyMusic);
+        }
 
-			Instance.musicSource.Stop();
-			Instance.musicSource.volume = baseMusicVolume;
-			Instance.musicSource.clip = musicClip;
-			Instance.musicSource.loop = true;
-			Instance.musicSource.Play();
+        public void PlayMusic(AudioClip musicClip)
+        {
+            if (musicClip.Equals(Instance.musicSource.clip))
+                return;
 
-		}
+            Instance.musicSource.Stop();
+            Instance.musicSource.volume = baseMusicVolume;
+            Instance.musicSource.clip = musicClip;
+            Instance.musicSource.loop = true;
+            Instance.musicSource.Play();
+        }
 
-		public void StopFadeOutMusic()
-		{
-			Instance.musicSource.DOFade(0f, 0.5f).SetUpdate(true).OnComplete(() =>
-			{
-				Instance.musicSource.Stop();
-				Instance.musicSource.clip = null;
-			});
-		}
+        public void StopFadeOutMusic()
+        {
+            Instance
+                .musicSource.DOFade(0f, 0.5f)
+                .SetUpdate(true)
+                .OnComplete(() =>
+                {
+                    Instance.musicSource.Stop();
+                    Instance.musicSource.clip = null;
+                });
+        }
 
-		public void FadeMusicVolume(float targetVolume, float duration = 0.5f)
-		{
-			Instance.musicSource.DOFade(targetVolume, duration).SetUpdate(true);
-		}
+        public void FadeMusicVolume(float targetVolume, float duration = 0.5f)
+        {
+            Instance.musicSource.DOFade(targetVolume, duration).SetUpdate(true);
+        }
 
-		public void FadeResetMusicVolume(float duration = 0.5f)
-		{
-			Instance.musicSource.DOFade(baseMusicVolume, duration).SetUpdate(true);
-		}
+        public void FadeResetMusicVolume(float duration = 0.5f)
+        {
+            Instance.musicSource.DOFade(baseMusicVolume, duration).SetUpdate(true);
+        }
 
-		public void FadeGameplayVolume(float targetVolume, float duration = 0.5f)
-		{
-			Instance.gameplaySource.DOFade(targetVolume, duration).SetUpdate(true);
-		}
+        public void FadeGameplayVolume(float targetVolume, float duration = 0.5f)
+        {
+            Instance.gameplaySource.DOFade(targetVolume, duration).SetUpdate(true);
+        }
 
-		public void FadeResetGameplayVolume(float duration = 0.5f)
-		{
-			Instance.gameplaySource.DOFade(baseGameplayVolume, duration).SetUpdate(true);
-		}
+        public void FadeResetGameplayVolume(float duration = 0.5f)
+        {
+            Instance.gameplaySource.DOFade(baseGameplayVolume, duration).SetUpdate(true);
+        }
 
+        //Gameplay
+        public void ResetGameplayPitch()
+        {
+            Instance.gameplaySource.pitch = 1f;
+        }
 
-		//Gameplay
-		public void ResetGameplayPitch()
-		{
-			Instance.gameplaySource.pitch = 1f;
-		}
+        public Dictionary<AudioSFX, float> EndTimeBySFX = new Dictionary<AudioSFX, float>();
 
-		public Dictionary<AudioSFX, float> EndTimeBySFX = new Dictionary<AudioSFX, float>();
-		public void PlayGameplayClip(AudioSFX sfx, AudioClip clip, float volumeScale = 1f)
-		{
-			if (!EndTimeBySFX.ContainsKey(sfx) || Time.time > EndTimeBySFX[sfx])
-			{
-				EndTimeBySFX[sfx] = Time.time + clip.length;
-			}
-			else if (sfx.onlyPlayOnceEachTime)
-			{
-				// We shouldnt play this SFX if one from the same type was recently played 
-				//Debug.Log("We should not play yet");
-				return;
-			}
-			Instance.gameplaySource.PlayOneShot(clip, volumeScale);
-		}
+        public void PlayGameplayClip(AudioSFX sfx, AudioClip clip, float volumeScale = 1f)
+        {
+            if (!EndTimeBySFX.ContainsKey(sfx) || Time.time > EndTimeBySFX[sfx])
+            {
+                EndTimeBySFX[sfx] = Time.time + clip.length;
+            }
+            else if (sfx.onlyPlayOnceEachTime)
+            {
+                // We shouldnt play this SFX if one from the same type was recently played
+                //Debug.Log("We should not play yet");
+                return;
+            }
+            Instance.gameplaySource.PlayOneShot(clip, volumeScale);
+        }
 
-		public void Mute(bool value)
-		{
-			//Debug.Log($"AudioController.Mute({value})");
+        public void Mute(bool value)
+        {
+            //Debug.Log($"AudioController.Mute({value})");
 
-			isMuted = value;
+            isMuted = value;
 
-			Instance.gameplaySource.mute = value;
-			Instance.musicSource.mute = value;
-		}
+            Instance.gameplaySource.mute = value;
+            Instance.musicSource.mute = value;
+        }
 
+        public void MuteMusic(bool value)
+        {
+            Instance.musicSource.mute = value;
+            MuteCheck();
+        }
 
-		public void MuteMusic(bool value)
-		{
-			Instance.musicSource.mute = value;
-			MuteCheck();
-		}
+        public void MuteSFX(bool value)
+        {
+            Instance.gameplaySource.mute = value;
+            OnMuteSFX?.Invoke(value);
+            MuteCheck();
+        }
 
-		public void MuteSFX(bool value)
-		{
-			Instance.gameplaySource.mute = value;
-			OnMuteSFX?.Invoke(value);
-			MuteCheck();
-		}
-
-
-		void MuteCheck()
-		{
-
-			if (Instance.gameplaySource.mute && Instance.musicSource.mute)
-			{
-				isMuted = true;
-			}
-			else
-			{
-				isMuted = false;
-			}
-		}
-	}
+        void MuteCheck()
+        {
+            if (Instance.gameplaySource.mute && Instance.musicSource.mute)
+            {
+                isMuted = true;
+            }
+            else
+            {
+                isMuted = false;
+            }
+        }
+    }
 }
--- Scripts/UI/UI_GlobalScoreDisplay.cs
+++ Scripts/UI/UI_GlobalScoreDisplay.cs
@@ -1,37 +1,47 @@
-using Blabbers.Game00;
-using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using Blabbers.Game00;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class UI_GlobalScoreDisplay : MonoBehaviour
 {
+    [BoxGroup("Configs")]
+    [SerializeField]
+    float startDelay = 0f;
 
-	[BoxGroup("Configs")] [SerializeField] float startDelay = 0f;
+    [Foldout("Components")]
+    [SerializeField]
+    GameObject display;
 
-	[Foldout("Components")][SerializeField] GameObject display;
-	[Foldout("Components")][SerializeField] UI_ProgressiveScoreDisplay control;
-	[SerializeField] int score;
+    [Foldout("Components")]
+    [SerializeField]
+    UI_ProgressiveScoreDisplay control;
 
-	// Start is called before the first frame update
-	void Start()
-	{
+    [SerializeField]
+    int score;
 
-		StartCoroutine(_Delay());
-		IEnumerator _Delay()
-		{
-			if(startDelay > 0) yield return new WaitForSeconds(startDelay);
-			else yield return new WaitForEndOfFrame();
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(_Delay());
+        IEnumerator _Delay()
+        {
+            if (startDelay > 0)
+                yield return new WaitForSeconds(startDelay);
+            else
+                yield return new WaitForEndOfFrame();
 
+            if (display != null)
+            {
+                display.SetActive(true);
+            }
 
-			display.SetActive(true);
-
-			score = ProgressController.GameProgress.score;
-			control.UpdateScore(score, Mathf.RoundToInt(score / 2));
-		}
-
-
-	}
-
-
+            score = ProgressController.GameProgress.score;
+            if (control)
+            {
+                control.UpdateScore(score, Mathf.RoundToInt(score / 2));
+            }
+        }
+    }
 }
